aeefb2b9b80b4216520a8c7bb549abcd6f7fabe1
Lakshmi Naarayanan <lakshminaarayanan.vaigaishrinivasan@cesltd.com>
2020-08-27 15:53:39 +0530

    set data

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,3 @@
-/*!
-    * Start Bootstrap - Grayscale v6.0.1 (https://startbootstrap.com/themes/grayscale)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-grayscale/blob/master/LICENSE)
-    */
     (function ($) {
     "use strict"; // Start of use strict
 
@@ -54,3 +49,54 @@
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
+
+
+function notifyUs(e) {
+    e.preventDefault();
+    var URL = "https://gmz7hhman7.execute-api.us-east-1.amazonaws.com/dev/notify-us";
+
+         // var Namere = /[A-Za-z]{1}[A-Za-z]/;
+         // if (!Namere.test($("#name").val())) {
+         //              alert ("Name can not less than 2 char");
+         //     return;
+         // }
+
+         if ($("#email").val()=="") {
+             alert ("Please enter your email id");
+             return;
+         }
+
+         var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+         if (!reeamil.test($("#email").val())) {
+             alert ("Please enter valid email address");
+             return;
+         }
+
+   //  var name = $("#name").val();
+    var email = $("#email").val();
+    var data = {
+       name : "no name",
+       client : "wesetrite",
+       email : email
+     };
+
+    $.ajax({
+      type: "POST",
+      url : "https://gmz7hhman7.execute-api.us-east-1.amazonaws.com/dev/notify-us", //   Maybe use URL?
+      dataType: "json",
+      crossDomain: "true",
+      contentType: "application/json; charset=utf-8",
+      data: JSON.stringify(data),
+
+      
+      success: function () {
+        // clear form and show a success message
+        alert("Response recorded. We will notify you once we go LIVE!");
+        document.getElementById("notify-form").reset();
+    location.reload();
+      },
+      error: function () {
+        // show an error message
+        alert("We are unable to process this request.");
+      }});
+  }
